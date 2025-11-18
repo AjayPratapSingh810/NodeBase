dff1c2b65b897f936d7a9a575fc1c5eb32373bdc
ajay pratap singh <pratapaj810@gmail.com>
2025-11-18 17:20:21 +0530

    setup prisma in project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
-const Page = () => {
-  return <div className="text-red-500">Welcome to the Home Page</div>;
-}
+import prisma from "@/lib/db";
+
+const Page = async () => {
+  const users = await prisma.user.findMany();
+  return (
+    <>
+      <div className="text-red-500">Welcome to the Home Page</div>
+      <div className="text-black-500">{JSON.stringify(users)}</div>
+    </>
+  );
+};
 
 export default Page;
