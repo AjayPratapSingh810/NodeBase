4ab4dcb541b664127430c4c414eb2e1e285a0791
ajay pratap singh <pratapaj810@gmail.com>
2025-12-04 19:18:28 +0530

    06 inngest Bg Jobs

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,13 @@
 import { z } from "zod";
 import { baseProcedure, createTRPCRouter, protectedProcedure } from "../init";
 import prisma from "@/lib/db";
+import { inngest } from "@/inngest/client";
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ ctx }) => {
+  getUsers: protectedProcedure.query(async ({ ctx }) => {
+    await inngest.send({
+      name: "test/hello.world",
+      data: { email: "abc@mail.com" },
+    });
     const userId = ctx.auth.user.id;
     return prisma.user.findMany({ where: { id: userId } });
   }),
