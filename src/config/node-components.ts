ac8d5e9490c0d03bad6cb73b719af40f571deca8
ajay pratap singh <pratapaj810@gmail.com>
2026-02-26 16:43:33 +0530

    complete flow of stripe execution node

--- src/config/node-components.ts
+++ src/config/node-components.ts
@@ -5,12 +5,14 @@ import { HttpRequestNode } from "@/features/executions/components/http-request/n
 import { ManualTriggerNode } from "@/features/triggers/components/manual-trigger/node";
 import { NodeType } from "@/generated/prisma/enums";
 import { GoogleFormTrigger } from "@/features/triggers/components/google-form-trigger/node";
+import { StripeTriggerNode } from "@/features/triggers/components/stripe-trigger/node";
 
 export const nodeComponents = {
   [NodeType.INITIAL]: InitialNode,
   [NodeType.HTTP_REQUEST]: HttpRequestNode,
   [NodeType.MANUAL_TRIGGER]: ManualTriggerNode,
   [NodeType.GOOGLE_FORM_TRIGGER]: GoogleFormTrigger, // TODO: Replace with actual google form trigger node when implemented
+  [NodeType.STRIPE_TRIGGER]: StripeTriggerNode, // TODO: Replace with actual stripe trigger node when implemented
 } as const satisfies NodeTypes;
 
 export type RegisteredNodeType = keyof typeof nodeComponents;
